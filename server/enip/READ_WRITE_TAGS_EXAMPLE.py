674de14adb796d9d75a7c1dc3fed0ea301542a60
karthhic <karthhic@users.noreply.github.com>
2017-05-08 13:54:52 +0300

    Update READ_WRITE_TAGS_EXAMPLE.py

--- server/enip/READ_WRITE_TAGS_EXAMPLE.py
+++ server/enip/READ_WRITE_TAGS_EXAMPLE.py
@@ -1,24 +1,39 @@
 #------PLC Tag Read and Write Code using CPPPO-----------
 #------Author:Karthhic Mukil-----------------------------
 from cpppo.server.enip import client
+import time
 import sys
+IP= "192.168.0.1"        # Ip Address of the PLC
 
-print(" ")
-print("Output:")
-timeout=1
+#------------------------------Functions------------------------------------------------------
+timeout=5
 port=44818
-def TagData(mode,tag_name, plc_addr):								                                                #Building a function
-    with client.connector( host = plc_addr, port = 44818, timeout = timeout ) as conn:     				                        #Creates a UDP connection
+def TagData(mode,tag_name, plc_addr):                                                                                                           #Building a function
+    with client.connector( host = plc_addr, port = 44818, timeout = timeout ) as conn:                                                          #Creates a UDP connection
         operations              = client.parse_operations( [tag_name] )
         if (mode == "wr"):
-            failures,transactions   = conn.process(operations=operations, depth=2, multiple=0,fragment=False, printing=False, timeout=timeout ) #Write Tag             
+            failures,transactions   = conn.process(operations=operations, depth=2, multiple=0,fragment=False, printing=False, timeout=timeout ) #Write Tag
         elif (mode == "rd"):
             for index,descr,op,reply,status,value in conn.pipeline(operations = operations, depth = 2 ):                                        #Read tag
-                pass
+                poz =(value[0])
             if value is None:
                 print("None returned while reading %s from PLC %s " % (tag_name, plc_addr))
-            return index,descr,op,reply,status,value[0]
+            return poz
 
+def PushButton(btag,secs):                                   # Function to trigger a value and reset in a specific interval, like a push button.
+    TagData("wr",str(btag)+"=(BOOL)True", IP)
+    time.sleep(secs)
+    TagData("wr",str(btag)+"=(BOOL)False", IP)
+
+def read(tag):                                               # Function to read a tag value.
+    tagout = TagData("rd",tag, IP)
+    return tagout
+
+def wdint(btag,val):                                         # Function to write a DINT value to a tag.
+    TagData("wr",str(btag)+"=(DINT)" + str(val), IP)
+
+def wbool(btag,state):                                       # Function to write a BOOL value to a tag.
+    TagData("wr",str(btag)+"=(BOOL)" + str(state), IP)
 #---------------------------Sample inputs----------------------------------
 #TagData("rd","Tag_Name", "192.168.0.1")
 #output = TagData("rd","Tag_Name", "192.168.0.2")
